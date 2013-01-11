31afb964f80e3c88b002bde30c283e206281a000
Daniel Egan <dje@jirvan.com>
2013-01-12 07:20:37 +1030

    Added JsonObjectMapper

---
+++ src/main/java/com/jirvan/util/JsonObjectMapper.java
@@ -0,0 +1,12 @@
+package com.jirvan.util;
+
+import com.jirvan.dates.*;
+import org.codehaus.jackson.map.*;
+
+public class JsonObjectMapper extends ObjectMapper {
+
+    public JsonObjectMapper() {
+        this.registerModule(Dates.getSerializerModule());
+    }
+
+}
