16aaf74fdb227d34abe1539f7f40345d8be1fcf2
Daniel Egan <djegan@casablanca.local>
2017-11-13 08:40:07 +1030

    Added TwoDecimalPlaceDeserializer.java and TwoDecimalPlaceSerializer.java

---
+++ src/main/java/com/jirvan/jackson/TwoDecimalPlaceDeserializer.java
@@ -0,0 +1,19 @@
+package com.jirvan.jackson;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.NumberDeserializers;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class TwoDecimalPlaceDeserializer extends NumberDeserializers.BigDecimalDeserializer {
+
+    @Override
+    public BigDecimal deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+        BigDecimal value = super.deserialize(p, ctxt);
+        return value == null ? null
+                             : value.setScale(2, BigDecimal.ROUND_UNNECESSARY);
+    }
+
+}
