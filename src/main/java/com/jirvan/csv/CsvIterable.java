216284e401ec4d32b66fd8c6c331d6979ad0ce2c
Daniel Egan <djegan@casablanca.local>
2019-07-20 11:58:23 +0930

    Added CsvIterable.java

---
+++ src/main/java/com/jirvan/csv/CsvIterable.java
@@ -0,0 +1,164 @@
+/*
+
+Copyright (c) 2019, Jirvan Pty Ltd
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+      this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+      this list of conditions and the following disclaimer in the documentation
+      and/or other materials provided with the distribution.
+    * Neither the name of Jirvan Pty Ltd nor the names of its contributors
+      may be used to endorse or promote products derived from this software
+      without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+package com.jirvan.csv;
+
+import com.jirvan.dates.Day;
+import com.jirvan.dates.Hour;
+import com.jirvan.dates.Millisecond;
+import com.jirvan.dates.Minute;
+import com.jirvan.dates.Month;
+import com.jirvan.dates.Second;
+import com.jirvan.util.Utl;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.Iterator;
+
+public class CsvIterable<T> implements Iterable<T> {
+
+    private Iterator iterator;
+    private Class<T> rowClass;
+
+    public CsvIterable(Class rowClass, InputStream inputStream) {
+        this.rowClass = rowClass;
+        this.iterator = new InternalIterator(inputStream);
+    }
+
+    public Iterator<T> iterator() {
+        return this.iterator;
+    }
+
+    private void validateHeader(CSVRecord headerRecord) {
+
+        int index = 0;
+        for (Field f : rowClass.getDeclaredFields()) {
+            if (headerRecord.size() < index + 1) {
+                throw new RuntimeException(String.format("Column header \"%s\" is missing", f.getName()));
+            }
+            String headerRecordHeadername = headerRecord.get(index++);
+            if (!headerRecordHeadername.equals(f.getName())) {
+                throw new RuntimeException(String.format("Invalid header \"%s\" (expected %s)", headerRecordHeadername, f.getName()));
+            }
+        }
+
+    }
+
+    private class InternalIterator implements Iterator {
+
+        private CSVParser csvParser;
+
+        public InternalIterator(InputStream inputStream) {
+            try {
+                csvParser = CSVFormat.EXCEL.withIgnoreEmptyLines()
+                                           .parse(new InputStreamReader(inputStream));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            CSVRecord header = csvParser.iterator().next();
+            validateHeader(header);
+        }
+
+        public boolean hasNext() {
+            return csvParser.iterator().hasNext();
+        }
+
+        public T next() {
+            try {
+                CSVRecord csvRecord = csvParser.iterator().next();
+                T row = rowClass.newInstance();
+                int index = 0;
+                for (Field field : rowClass.getDeclaredFields()) {
+                    if (csvRecord.size() < index + 1) {
+                        throw new RuntimeException(String.format("Row %d: value for column \"%s\" is missing", csvRecord.getRecordNumber(), field.getName()));
+                    }
+                    String value = csvRecord.get(index++);
+                    try {
+                        setFieldValue(row, field, value);
+                    } catch (Throwable t) {
+                        throw new RuntimeException(String.format("Row %d, column \"%s\": %s", csvRecord.getRecordNumber(), field.getName(), Utl.coalesce(t.getMessage(), t.getClass().getSimpleName())), t);
+                    }
+                }
+                return row;
+            } catch (InstantiationException | IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    private void setFieldValue(T row, Field field, String stringValue) throws IllegalAccessException {
+        if (field.getType() == String.class) {
+            field.set(row, stringValue);
+        } else if (field.getType() == Integer.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : Integer.parseInt(stringValue.trim()));
+        } else if (field.getType() == Long.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : Long.parseLong(stringValue.trim()));
+        } else if (field.getType() == BigDecimal.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : new BigDecimal(stringValue.trim()));
+        } else if (field.getType() == Boolean.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : Boolean.parseBoolean(stringValue.trim()));
+        } else if (field.getType() == Month.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : Month.fromString(stringValue.trim()));
+        } else if (field.getType() == Day.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : Day.fromString(stringValue.trim()));
+        } else if (field.getType() == LocalDate.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : LocalDate.parse(stringValue.trim()));
+        } else if (field.getType() == LocalDateTime.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : LocalDateTime.parse(stringValue.trim()));
+        } else if (field.getType() == Hour.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : Hour.fromString(stringValue.trim()));
+        } else if (field.getType() == Minute.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : Minute.fromString(stringValue.trim()));
+        } else if (field.getType() == Second.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : Second.fromString(stringValue.trim()));
+        } else if (field.getType() == Millisecond.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : Millisecond.fromString(stringValue.trim()));
+        } else if (field.getType() == ZonedDateTime.class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : ZonedDateTime.parse(stringValue.trim()));
+        } else if (Enum.class.isAssignableFrom(field.getType())) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : Enum.valueOf((Class<? extends Enum>) field.getType(), stringValue.trim()));
+        } else if (field.getType() == byte[].class) {
+            field.set(row, stringValue == null || stringValue.trim().length() == 0 ? null : stringValue.getBytes());
+        } else {
+            throw new UnsupportedOperationException(String.format("Cannot process values of type \"%s\"", field.getType().getName()));
+        }
+    }
+
+}
